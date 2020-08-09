a829db4134b2f5162d44f6329aab8145e71cc0fb
Azizbek Xushvaqtov <azizbekx2702@gmail.com>
2020-08-09 08:30:50 +0500

    Enter all

---
+++ critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleController.java
@@ -0,0 +1,75 @@
+package com.udacity.jdnd.course3.critter.schedule;
+
+import com.udacity.jdnd.course3.critter.entities.Employee;
+import com.udacity.jdnd.course3.critter.entities.Pet;
+import com.udacity.jdnd.course3.critter.entities.Schedule;
+import com.udacity.jdnd.course3.critter.service.PetService;
+import com.udacity.jdnd.course3.critter.service.ScheduleService;
+import com.udacity.jdnd.course3.critter.service.UserService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Handles web requests related to Schedules.
+ */
+@RestController
+@RequestMapping("/schedule")
+public class ScheduleController {
+    @Autowired
+    private ScheduleService scheduleService;
+    @Autowired
+    private PetService petService;
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ScheduleDTO createSchedule(@RequestBody ScheduleDTO scheduleDTO) {
+        Schedule schedule = new Schedule();
+        BeanUtils.copyProperties(scheduleDTO, schedule);
+        return setScheduleDTO(scheduleService.createSchedule(schedule));
+    }
+
+    @GetMapping
+    public List<ScheduleDTO> getAllSchedules() {
+        List<Schedule> schedules = scheduleService.getAllSchedules();
+        return schedules.stream().map(this::setScheduleDTO).collect(Collectors.toList());
+
+    }
+
+    @GetMapping("/pet/{petId}")
+    public List<ScheduleDTO> getScheduleForPet(@PathVariable long petId) {
+        List<Schedule> schedules = petService.findById(petId).getSchedules();
+        return schedules.stream().map(this::setScheduleDTO).collect(Collectors.toList());
+    }
+
+    @GetMapping("/employee/{employeeId}")
+    public List<ScheduleDTO> getScheduleForEmployee(@PathVariable long employeeId) {
+        Employee employee = userService.getEmployeeById(employeeId);
+        if (employee.getSchedules() == null) return null;
+        List<Schedule> schedules = employee.getSchedules();
+
+        return schedules.stream().map(this::setScheduleDTO).collect(Collectors.toList());
+    }
+
+    @GetMapping("/customer/{customerId}")
+    public List<ScheduleDTO> getScheduleForCustomer(@PathVariable long customerId) {
+        List<Schedule> schedules = userService.findById(customerId).getSchedule();
+        return schedules.stream().map(this::setScheduleDTO).collect(Collectors.toList());
+    }
+
+    public ScheduleDTO setScheduleDTO(Schedule schedule){
+        ScheduleDTO scheduleDTO = new ScheduleDTO();
+        BeanUtils.copyProperties(schedule, scheduleDTO);
+        List<Long> employeeId = schedule.getEmployee().stream().map(Employee::getId).collect(Collectors.toList());
+        List<Long> petId = schedule.getPet().stream().map(Pet::getId).collect(Collectors.toList());
+        scheduleDTO.setEmployeeIds(employeeId);
+        scheduleDTO.setPetIds(petId);
+
+
+        return scheduleDTO;
+    }
+}
